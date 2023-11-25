ef37bca9a044cbe8c7b7292af83d80a1025d6b29
AbdeelBrh <abdelilah.bourhou@gmail.com>
2023-11-25 12:48:19 +0100

    Update error handling

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -16,6 +16,7 @@ func ListCars(s *server.Server) http.HandlerFunc {
 		var cars []model.Car
 		s.ParkingLotService.DB.Find(&cars)
 
+		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(cars)
 	}
 }
@@ -27,7 +28,7 @@ func AddCar(s *server.Server) http.HandlerFunc {
 		var car model.Car
 		if err := json.NewDecoder(r.Body).Decode(&car); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Invalid request payload"})
+			json.NewEncoder(w).Encode(ErrorResponse{"Invalid request payload"})
 			return
 		}
 
@@ -35,27 +36,28 @@ func AddCar(s *server.Server) http.HandlerFunc {
 
 		if exists {
 			w.WriteHeader(http.StatusConflict)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Car already exists"})
+			json.NewEncoder(w).Encode(ErrorResponse{"Car already exists"})
 			return
 
 		} else if car.Mileage < 0 {
 			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Mileage parameter must be positive"})
+			json.NewEncoder(w).Encode(ErrorResponse{"Mileage parameter must be positive"})
 			return
 
 		} else if car.CarModel == "" || car.Registration == "" {
 			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Car model and registration are required"})
+			json.NewEncoder(w).Encode(ErrorResponse{"Car model and registration are required"})
 			return
 		}
 
 		car.Available = true
 		if err := s.ParkingLotService.DB.Create(&car).Error; err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Failed to create car"})
+			json.NewEncoder(w).Encode(ErrorResponse{"Failed to create car"})
 			return
 		}
 
+		w.WriteHeader(http.StatusCreated)
 		json.NewEncoder(w).Encode(car)
 	}
 }
@@ -70,19 +72,20 @@ func RentCar(s *server.Server) http.HandlerFunc {
 
 		if !exists {
 			w.WriteHeader(http.StatusNotFound)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Car not found"})
+			json.NewEncoder(w).Encode(ErrorResponse{"Car not found"})
 			return
 		}
 
 		if !car.Available {
 			w.WriteHeader(http.StatusConflict)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Car is not available"})
+			json.NewEncoder(w).Encode(ErrorResponse{"Car is not available"})
 			return
 		}
 
 		car.Available = false
 		s.ParkingLotService.DB.Save(&car)
 
+		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode("The Car with registration " + registration + " is rented!")
 	}
 }
@@ -101,25 +104,25 @@ func ReturnCar(s *server.Server) http.HandlerFunc {
 
 		if !exists {
 			w.WriteHeader(http.StatusNotFound)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Car not found"})
+			json.NewEncoder(w).Encode(ErrorResponse{"Car not found"})
 			return
 		}
 
 		if car.Available {
 			w.WriteHeader(http.StatusConflict)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Car is already available"})
+			json.NewEncoder(w).Encode(ErrorResponse{"Car is already available"})
 			return
 		}
 
 		var payload KilometersPayload
 		if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Invalid kilometers payload"})
+			json.NewEncoder(w).Encode(ErrorResponse{"Invalid kilometers payload"})
 			return
 
 		} else if payload.Kilometers < 0 {
 			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Kilometers parameter must be positive"})
+			json.NewEncoder(w).Encode(ErrorResponse{"Kilometers parameter must be positive"})
 			return
 		}
 
@@ -127,6 +130,7 @@ func ReturnCar(s *server.Server) http.HandlerFunc {
 		car.Mileage += float64(payload.Kilometers)
 		s.ParkingLotService.DB.Save(&car)
 
+		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(car)
 	}
 }
@@ -141,10 +145,11 @@ func GetCar(s *server.Server) http.HandlerFunc {
 
 		if !exists {
 			w.WriteHeader(http.StatusNotFound)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Car not found"})
+			json.NewEncoder(w).Encode(ErrorResponse{"Car not found"})
 			return
 		}
 
+		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(car)
 	}
 }
@@ -159,12 +164,17 @@ func DeleteCar(s *server.Server) http.HandlerFunc {
 
 		if !exists {
 			w.WriteHeader(http.StatusNotFound)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Car not found"})
+			json.NewEncoder(w).Encode(ErrorResponse{"Car not found"})
 			return
 		}
 
 		s.ParkingLotService.DB.Delete(&car)
 
+		w.WriteHeader(http.StatusNoContent)
 		json.NewEncoder(w).Encode("The Car with registration " + registration + " is deleted!")
 	}
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
 }
